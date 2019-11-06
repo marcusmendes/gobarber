6125e9154688b8f4aaf323f71165ee078e9ba435
Marcus Maciel <marcusmaciel@controllab>
2019-11-06 10:01:08 -0300

    Projeto GoBarber

---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+// Serviços de e-mail
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril
+// Mailtrap (dev)
+
+export default {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
